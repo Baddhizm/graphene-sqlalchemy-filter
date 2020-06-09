abe6f9f76174f4e0f3c13830699e0c9df3abb89a
baddhizm <frenggy@gmail.com>
2020-06-09 23:36:25 +0300

    add fix encoding

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
 
-with open('graphene_sqlalchemy_filter/__init__.py', 'rb', encoding='utf-8') as f:
+with open('graphene_sqlalchemy_filter/__init__.py', 'rb') as f:
     version = str(
         ast.literal_eval(_version_re.search(f.read().decode('utf-8')).group(1))
     )
